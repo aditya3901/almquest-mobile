a2f10055155ac22b4980e328c081f6da4e1165ad
Tamonash <tamonashbhattacharyya2@gmail.com>
2022-12-28 19:15:24 +0530

    Team Home edit lineicon added

--- lib/screens/team.dart
+++ lib/screens/team.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import '../widgets/navigation_drawer.dart';
+import 'package:almquest/utils/constants.dart';
+import 'package:almquest/screens/team_body.dart';
 
 class Team extends StatelessWidget {
   const Team({Key? key}) : super(key: key);
@@ -7,16 +9,15 @@ class Team extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "AlmQuest",
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
+        appBar: AppBar(
+          title: const Text(
+            "AlmQuest",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
           ),
         ),
-      ),
-      drawer: NavigationDrawer(),
-      body: Container(),
-    );
+        drawer: NavigationDrawer(),
+        body: const TeamBody());
   }
 }
